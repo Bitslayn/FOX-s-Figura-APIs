bc0fdd2745f3aefe9e078d5eb711eb68aa3e244d
Bitslayn <96226175+Bitslayn@users.noreply.github.com>
2025-05-08 16:20:56 -0400

    Unbump the version :shrug:

--- FOXCamera/FOXCamera.lua
+++ FOXCamera/FOXCamera.lua
@@ -3,7 +3,7 @@ ____  ___ __   __
 | __|/ _ \\ \ / /
 | _|| (_) |> w <
 |_|  \___//_/ \_\
-FOX's Camera API v1.5.1
+FOX's Camera API v1.5.0
 
 Recommended Figura 0.1.6 or Goofy Plugin
 Supports 0.1.5 without pre_render with the built-in compatibility mode

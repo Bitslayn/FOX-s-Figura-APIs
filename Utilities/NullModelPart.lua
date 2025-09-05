45224294f23ae2d5e13038a446a0500fbfabf5b6
Bitslayn <96226175+Bitslayn@users.noreply.github.com>
2025-09-05 16:41:56 -0400

    NMP v0.1.0

---
+++ Utilities/NullModelPart.lua
@@ -0,0 +1,79 @@
+--[[
+____  ___ __   __
+| __|/ _ \\ \ / /
+| _|| (_) |> w <
+|_|  \___//_/ \_\
+FOX's Better ModelPart Errors v0.1.0
+]]
+
+---Custom error message for calling a method on NullModelParts
+local err = [=[ModelPart path is incorrect. Underlined does not exist in non-underlined
+
+%s
+script:
+%s§c<--[Here]
+
+(Below contains traceback for method called on NullModelPart)]=]
+
+---@class ModelPart
+local ModelPart = figuraMetatables.ModelPart
+local MP_i = ModelPart.__index
+
+---@class NullModelPart: ModelPart
+local NullModelPart_i = {
+	__type = "NullModelPart",
+	__index = function(self, key)
+		if type(MP_i(models, key)) ~= "function" then return self end
+
+		local priv = self[1]
+		error(err:format(priv.stack, priv.script), 2)
+	end,
+	__metatable = nil,
+}
+
+function ModelPart:__index(key)
+	local valid = MP_i(self, key)
+	if valid then return valid end
+
+	-- Get traceback and line number of invalid indexed part
+
+	---@type string, string
+	local trace, stack = select(2, pcall(function() error("", 4) end)):match("([^\n]-)\n(.*)")
+	---@type string, string
+	local scriptPath, scriptLine = trace:match("(.*):([%d]*)")
+
+	scriptPath = scriptPath:gsub("/", ".")
+	scriptLine = tonumber(scriptLine)
+
+	-- Get script where invalid part was indexed
+
+	local buffer = data:createBuffer()
+	for _, v in pairs(avatar:getNBT().scripts[scriptPath]) do
+		buffer:write(v)
+	end
+	buffer:setPosition(0)
+	local decoded = buffer:readByteArray()
+	buffer:close()
+
+	-- Get exact line from script where invalid part was indexed
+
+	local rawScript
+	local i = 0
+	for v in decoded:gmatch("[^\n]*") do
+		i = i + 1
+		if i == scriptLine then rawScript = "  " .. v end
+	end
+
+	-- Underline invalid index
+
+	local part = self
+	local script = rawScript:gsub("([.%[])", function(sep)
+		if not part then return end
+		part = part:getParent()
+		return part and sep or "§n" .. sep
+	end) .. "§c"
+
+	-- Create and return NullModelPart only if organically indexed
+
+	return script:find("[=(,]%s*models") and setmetatable({ [1] = { stack = stack, script = script } }, NullModelPart_i)
+end

35d24a4f99b3106f919836265c7526f3236fe652
Bitslayn <96226175+Bitslayn@users.noreply.github.com>
2025-09-05 19:49:09 -0400

    NMP v0.2.0

--- Utilities/NullModelPart.lua
+++ Utilities/NullModelPart.lua
@@ -3,17 +3,27 @@ ____  ___ __   __
 | __|/ _ \\ \ / /
 | _|| (_) |> w <
 |_|  \___//_/ \_\
-FOX's Better ModelPart Errors v0.1.2
+FOX's Better ModelPart Errors v0.2.0
 ]]
 
----Custom error message for calling a method on NullModelParts
-local err = [=[ModelPart path is incorrect. Underlined does not exist in non-underlined
+---@param priv NullModelPart.*INTERNAL*
+local function buildError(priv)
+	local formatting = "§7" .. priv.index[1]
+	for i = 2, #priv.index do
+		local v = priv.index[i]
+		formatting = formatting .. (v:find("%s") and '["%s"]' or ".%s"):format(v)
 
-%s
-script:
-%s§o<--[Here]§c
+		if i == priv.validDepth then
+			formatting = formatting .. "§c§n"
+		end
+	end
+	formatting = formatting .. "§c"
 
-(Below contains additional information)]=]
+	local err =
+	'ModelPart path is incorrect. "%s" is not a child of "%s". Please check spelling and capitalization \n\n%s§o<--[Here]§c\n\n%s\nscript:\n  %s\n\n(Below contains additional information)'
+
+	error(err:format(priv.index[priv.validDepth + 1], priv.index[priv.validDepth], formatting, priv.stack, priv.script), 3)
+end
 
 ---@class ModelPart
 local ModelPart = figuraMetatables.ModelPart
@@ -23,13 +33,18 @@ local MP_i = ModelPart.__index
 local NullModelPart_i = {
 	__type = "NullModelPart",
 	__index = function(self, key)
+		---@class NullModelPart.*INTERNAL*
 		local priv = self[1]
-		if type(MP_i(models, key)) ~= "function" and priv.depth < 16 then
-			priv.depth = priv.depth + 1
+
+		-- Add key to index if the indexed is not a function
+
+		if type(MP_i(models, key)) ~= "function" and priv.invalidDepth < 16 then
+			priv.invalidDepth = priv.invalidDepth + 1
+			table.insert(priv.index, key)
 			return self
 		end
 
-		error(err:format(priv.stack, priv.script), 2)
+		buildError(priv)
 	end,
 	__metatable = nil,
 }
@@ -38,12 +53,15 @@ function ModelPart:__index(key)
 	local valid = MP_i(self, key)
 	if valid then return valid end
 
+	---@class NullModelPart.*INTERNAL*
+	local priv = {}
+
 	-- Get traceback and line number of invalid indexed part
 
 	---@type string, string
-	local trace, stack = select(2, pcall(function() error("", 4) end)):match("([^\n]-)\n(.*)")
+	priv.trace, priv.stack = select(2, pcall(function() error("", 4) end)):match("([^\n]-)\n(.*)")
 	---@type string, string
-	local scriptPath, scriptLine = trace:match("(.*):([%d]*)")
+	local scriptPath, scriptLine = priv.trace:match("(.*):([%d]*)")
 
 	scriptPath = scriptPath:gsub("/", ".")
 	scriptLine = tonumber(scriptLine)
@@ -60,24 +78,34 @@ function ModelPart:__index(key)
 
 	-- Get exact line from script where invalid part was indexed
 
-	local rawScript
 	local i = 0
 	for v in decoded:gmatch("[^\n]*") do
 		i = i + 1
-		if i == scriptLine then rawScript = "  " .. v:gsub("%s*$", "") end
+		if i == scriptLine then priv.script = v:gsub("%s*$", "") end
 	end
 
-	-- Underline invalid index
+	-- Build index tree
 
+	---@type number
+	priv.validDepth = 0
+	---@type string[]
+	priv.index = {}
 	local part = self
-	local script = rawScript:gsub("([.%[])", function(sep)
-		if not part then return end
+	while part do
+		table.insert(priv.index, 1, part:getName())
 		part = part:getParent()
-		return part and sep or "§n" .. sep
-	end) .. "§c"
+		priv.validDepth = priv.validDepth + 1
+	end
+
+	---@type number
+	priv.invalidDepth = 1
+	table.insert(priv.index, key)
 
 	-- Create and return NullModelPart only if organically indexed
 
-	return script:find("[=(,]%s*models") and
-	setmetatable({ [1] = { stack = stack, script = script, depth = 1 } }, NullModelPart_i)
+	if not priv.script:find("[=(,]%s*models") then return end
+	return setmetatable({ [1] = priv }, NullModelPart_i)
 end
+
+local a, b = models.Models.Player.root["A B"], models.Models.Player.Root
+print(a:rot(), b:rot())
